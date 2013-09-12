b579c05d0a100783daa50ce72fa414fb05e8cae9
brycesenz <bryce.senz@gmail.com>
2013-09-11 21:49:57 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snicker::Application.config.secret_token = '0a96ae420af0026d23e882e114d32a6d899b98c8898e44ac6d908dcbb6355a14be4326ffb1fd8b126ad9182a8d87eb5fa9675654025b83a160e2777266074039'
